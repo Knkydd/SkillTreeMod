6a49d55c039a8d2c20a11dfc1e2e4adde8298d13
MakaronynaZavtrak <semyonov.owa@gmail.com>
2025-05-28 22:54:23 +0500

    Сделал текстуры для классов специальностей. Пока всё растянуто, нужно будет подправлять

--- src/main/java/com/example/skilltreemod/gui/other/TankContainer.java
+++ src/main/java/com/example/skilltreemod/gui/other/TankContainer.java
@@ -2,7 +2,9 @@ package com.example.skilltreemod.gui.other;
 
 import com.example.skilltreemod.gui.subscreens.TankClassScreen;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.resources.ResourceLocation;
 
 public class TankContainer extends ClassContainer {
     public TankContainer(int x, int y, int width, int height, Screen parentScreen){
@@ -12,5 +14,11 @@ public class TankContainer extends ClassContainer {
     @Override
     public void onClick(double mouseX, double mouseY) {
         Minecraft.getInstance().setScreen(new TankClassScreen(parentScreen));
+    }
+
+    @Override
+    public void renderBackground(GuiGraphics guiGraphics) {
+        ResourceLocation texture = new ResourceLocation("skilltreemod", "textures/gui/tank_background.jpg");
+        guiGraphics.blit(texture, this.getX(), this.getY(), 0, 0, this.width, this.height, this.width, this.height);
     }
 }
